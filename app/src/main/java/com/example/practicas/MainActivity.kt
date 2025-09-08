fb1e3e0ed93313d1a7591f8666799e3b708475cf
Chr17s <crishdz690@gmail.com>
2025-09-08 14:11:46 -0600

    Terminado

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.example.practicas
 
+import android.graphics.fonts.FontFamily
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.createFontFamilyResolver
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicas.ui.theme.PracticasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,30 +42,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        //REPOSITORIO DE PRACTICAS
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    Botones()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PracticasTheme {
-        Greeting("Android")
+fun Botones() {
+    val texto by remember { mutableStateOf("Enviado") }
+    val context = LocalContext.current
+    var Nombre by remember { mutableStateOf("") }
+    Column (modifier = Modifier.fillMaxWidth(1f),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(modifier= Modifier.padding(10.dp)) {
+            Text("Nombre",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Blue
+                )
+            )
+        }
+        Row(modifier = Modifier.padding(10.dp)) {
+            TextField(
+                value = Nombre, onValueChange = { nuevoTexto -> Nombre = nuevoTexto} )
+        }
+        Row (modifier = Modifier.padding(20.dp))
+        {
+            Button(onClick = {Toast.makeText(
+                context, texto, Toast.LENGTH_LONG
+            ).show()},colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Green
+                )) {
+                Text("Enviar")
+            }
+        }
     }
 }
