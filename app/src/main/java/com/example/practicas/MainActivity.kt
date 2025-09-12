985ce8bc8c7b306c6f42a4663adab0c8a9b1ee66
Chr17s <crishdz690@gmail.com>
2025-09-11 23:54:05 -0600

    Calculadora hecha con validaciones, faltan ultimos retoques en la UI

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.practicas
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -55,9 +57,14 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun Calculadora() {
+    //Variables para guardar los numeros de la operacion
+    var num1 by remember { mutableStateOf("") }
+    var num2 by remember { mutableStateOf("") }
     //variables para mostrar la operación y el resultado
-    var operacion by remember{ mutableStateOf("") }
+    var operador by remember{ mutableStateOf<Int?>(null) }
+    var operacion by remember { mutableStateOf("") }
     var resultado by remember { mutableStateOf("") }
+
 
     Column(
         modifier = Modifier
@@ -68,7 +75,8 @@ fun Calculadora() {
     ) {
         //TEXTO QUE MUESTRA EL RESULTADO
         Text(
-            text = resultado, fontSize = 50.sp,
+            text = if (resultado.isNotEmpty()) resultado else num2.ifEmpty { num1 },
+            fontSize = 50.sp,
             modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp),
             textAlign = TextAlign.End
         )
@@ -84,13 +92,20 @@ fun Calculadora() {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedButton(
-                onClick = { operacion = ""; resultado = "0" },
+                onClick = { num1 = ""
+                    num2 = ""
+                    operador = null
+                    resultado = ""
+                },
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "AC", fontSize = 45.sp, color = Color.Black)
             }
             OutlinedButton(
-                onClick = { operacion = "/" },
+                onClick = { operador = 4
+                    num1 = num2
+                    num2 = ""
+                    operacion += "/"},
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "/", fontSize = 45.sp)
@@ -102,19 +117,25 @@ fun Calculadora() {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedButton(
-                onClick = { operacion += "1" },
+                onClick = { num2 += "1"
+                            operacion +=1 },
                 modifier = Modifier.weight(1f)
             ) { Text("1", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "2" },
+                onClick = { num2 += "2"
+                            operacion +=2 },
                 modifier = Modifier.weight(1f)
             ) { Text("2", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "3" },
+                onClick = { num2 += "3"
+                            operacion += 3 },
                 modifier = Modifier.weight(1f)
             ) { Text("3", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "x" },
+                onClick = { operador = 3
+                    num1 = num2
+                    num2 = ""
+                    operacion += "x"},
                 modifier = Modifier.weight(1f)
             ) { Text("X", fontSize = 45.sp, color = Color.Black) }
         }
@@ -124,19 +145,25 @@ fun Calculadora() {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedButton(
-                onClick = { operacion += "4" },
+                onClick = { num2 += "4"
+                            operacion +=4 },
                 modifier = Modifier.weight(1f)
             ) { Text("4", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "5" },
+                onClick = { num2 += "5"
+                            operacion +=5 },
                 modifier = Modifier.weight(1f)
             ) { Text("5", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "6" },
+                onClick = { num2 += "6"
+                            operacion +=6 },
                 modifier = Modifier.weight(1f)
             ) { Text("6", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "-" },
+                onClick = { operador = 2
+                    num1 = num2
+                    num2 = ""
+                    operacion += "-"},
                 modifier = Modifier.weight(1f)
             ) { Text("-", fontSize = 45.sp, color = Color.Black) }
         }
@@ -146,19 +173,25 @@ fun Calculadora() {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedButton(
-                onClick = { operacion += "7" },
+                onClick = { num2 += "7"
+                            operacion +=7 },
                 modifier = Modifier.weight(1f)
             ) { Text("7", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "8" },
+                onClick = { num2 += "8"
+                            operacion +=8 },
                 modifier = Modifier.weight(1f)
             ) { Text("8", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "9" },
+                onClick = { num2 += "9"
+                            operacion +=9 },
                 modifier = Modifier.weight(1f)
             ) { Text("9", fontSize = 45.sp, color = Color.Black) }
             OutlinedButton(
-                onClick = { operacion += "+" },
+                onClick = { operador = 1
+                    num1 = num2
+                    num2 = ""
+                    operacion += "+"},
                 modifier = Modifier.weight(1f)
             ) { Text("+", fontSize = 45.sp, color = Color.Black) }
         }
@@ -167,18 +200,47 @@ fun Calculadora() {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
+            // Punto decimal
             OutlinedButton(
-                onClick = { operacion += "." },
+                onClick = {
+                    if (!num2.contains(".")) {   // evita múltiples puntos
+                        num2 = if (num2.isEmpty())
+                        "0." else num2 + "."
+                        operacion += "."
+                    }
+                },
                 modifier = Modifier.weight(1f)
             ) { Text(".", fontSize = 45.sp, color = Color.Black) }
+
+            // 0
             OutlinedButton(
-                onClick = { operacion += "0" },
+                onClick = { num2 += "0" },
                 modifier = Modifier.weight(1f)
             ) { Text("0", fontSize = 45.sp, color = Color.Black) }
-            OutlinedButton(onClick = { resultado = "Resultado" }, modifier = Modifier.weight(2f)) {
-                Text("=", fontSize = 45.sp, color = Color.Black)
-            }
-        }
+
+            // =
+            Button(
+                onClick = {
+                    val n1 = num1.toDoubleOrNull() ?: 0.0
+                    val n2 = num2.toDoubleOrNull() ?: 0.0
+                    val respuesta = when (operador) {
+                        1 -> n1 + n2
+                        2 -> n1 - n2
+                        3 -> n1 * n2
+                        4 -> if (n2 != 0.0) n1 / n2 else Double.NaN //Not A Number
+                        else -> n2
+                    }
+
+                    //limitarlo a 4 decimales
+                    val decimales = DecimalFormat("#.####")
+                    resultado = decimales.format(respuesta)
+                    operacion = ""
+                    num1 = ""
+                    num2 = ""
+                    operador = null
+                },
+                modifier = Modifier.weight(2f)
+            ) { Text("=", fontSize = 45.sp, color = Color.Black) }
     }
     Column(
         modifier = Modifier.fillMaxWidth().padding(16.dp),
@@ -187,4 +249,5 @@ fun Calculadora() {
     ){
 
     }
+}
 }
